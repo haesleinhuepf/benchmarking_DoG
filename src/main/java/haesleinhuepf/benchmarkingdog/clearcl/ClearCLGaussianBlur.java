339909df48a1c68a822910fa811d63134e568204
haesleinhuepf <rhaase@mpi-cbg.de>
2017-09-07 21:50:18 +0200

    add workaround for watchdog issue

--- src/main/java/haesleinhuepf/benchmarkingdog/clearcl/ClearCLGaussianBlur.java
+++ src/main/java/haesleinhuepf/benchmarkingdog/clearcl/ClearCLGaussianBlur.java
@@ -61,7 +61,31 @@ public class ClearCLGaussianBlur extends OpsBase
     mConvolutionKernelImage2F.setArgument("output", output);
     mConvolutionKernelImage2F.setArgument("radius", mRadius);
     mConvolutionKernelImage2F.setGlobalSizes(input.getDimensions());
-    mConvolutionKernelImage2F.run();
+    //mConvolutionKernelImage2F.run();
+
+    long[] sizes = new long[input.getDimensions().length];
+    long[] offsets = new long[input.getDimensions().length];
+    for (int i = 0; i < sizes.length; i++) {
+      sizes[i] = input.getDimensions()[i];
+      offsets[i] = 0;
+    }
+
+    long originalSize0 = sizes[0];
+    int numberOfSplits = 32;
+    for (int j = 0; j < numberOfSplits; j++) {
+      if (j < numberOfSplits - 1) {
+        sizes[0] = originalSize0 / numberOfSplits;
+      } else {
+        sizes[0] = originalSize0 - offsets[0];
+      }
+
+      //System.out.println("s offset: " + Arrays.toString(offsets));
+      //System.out.println("s sizes: " + Arrays.toString(sizes));
+      mConvolutionKernelImage2F.setGlobalSizes(sizes);
+      mConvolutionKernelImage2F.setGlobalOffsets(offsets);
+      mConvolutionKernelImage2F.run();
+      offsets[0] += sizes[0];
+    }
 
     return output;
   }
